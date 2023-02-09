cd32ebac30db58df94078229270cfc2491104e10
09009 <87968927+haaland09009@users.noreply.github.com>
2023-02-09 22:33:56 +0900

    Add files via upload

---
+++ 위클리 챌린지/부족한 금액 계산하기.py
@@ -0,0 +1,10 @@
+def solution(price, money, count):
+    result = 0
+    for i in range(1, count+1):
+        result += price * i
+    
+    if result >= money:
+        return result - money
+    else:
+        return 0
+        
