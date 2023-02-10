b24e1c6791d2747d609b1841afe07961ceb4c7b0
09009 <87968927+haaland09009@users.noreply.github.com>
2023-02-10 13:20:59 +0900

    Update 3진법 뒤집기.py

--- 월간 코드 챌린지/3진법 뒤집기.py
+++ 월간 코드 챌린지/3진법 뒤집기.py
@@ -9,3 +9,16 @@ def solution(n):
         answer += (int(tmp[i]) * 3**(len(tmp)-1-i))
 
     return answer
+
+-------------------------------------------------------
+
+def solution(n):
+    answer = 0
+    tmp = ''
+    while n != 0:
+        tmp += str(n % 3)
+        n = n // 3
+
+    return int(tmp, 3)
+
+# int(x,3) : 3진법으로 구성된 x를 10진법으로 바꿔주는 역할을 한다.
