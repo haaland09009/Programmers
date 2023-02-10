8e97969ac09cd547c0ddf15f92f2e0f11e9e7a03
09009 <87968927+haaland09009@users.noreply.github.com>
2023-02-10 15:12:35 +0900

    Update 숫자 문자열과 영단어.py

--- 카카오 채용연계형 인턴십/숫자 문자열과 영단어.py
+++ 카카오 채용연계형 인턴십/숫자 문자열과 영단어.py
@@ -14,3 +14,14 @@ def solution(s):
             tmp = ''
             
     return int(answer)
+
+
+
+
+def solution(s):
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for i in range(len(words)):
+        s = s.replace(words[i], str(i))
+
+    return int(s)
