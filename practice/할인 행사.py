b1c7385d3b357070acd0aca5b322e79f4a8676b9
09009 <87968927+haaland09009@users.noreply.github.com>
2023-02-25 17:12:25 +0900

    Update 할인 행사.py

--- practice/할인 행사.py
+++ practice/할인 행사.py
@@ -15,21 +15,21 @@ def solution(want, number, discount):
 
 #---------------------------------
 # 직접 해결한 풀이
-from collections import Counter
-def solution(want, number, discount):
-    answer = 0
+# from collections import Counter
+# def solution(want, number, discount):
+#     answer = 0
     
-    dic = {}
-    for i,j in zip(want, number):
-        dic[i] = j
+#     dic = {}
+#     for i,j in zip(want, number):
+#         dic[i] = j
         
-    for i in range(len(discount)):
-        tmp = Counter(discount[i:i+10])
-        count = 0
-        for k,v in dic.items():
-            if dic[k] <= tmp[k]: 
-                count += 1
-        if count == len(want):
-            answer += 1
+#     for i in range(len(discount)):
+#         tmp = Counter(discount[i:i+10])
+#         count = 0
+#         for k,v in dic.items():
+#             if dic[k] <= tmp[k]: 
+#                 count += 1
+#         if count == len(want):
+#             answer += 1
  
-    return answer
+#     return answer
