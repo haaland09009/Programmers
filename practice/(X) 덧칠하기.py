f1f905b15e5efd716c51e882d33ebc7d6336ff4c
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-03-04 17:44:15 +0900

    Update (X) 덧칠하기.py

--- practice/(X) 덧칠하기.py
+++ practice/(X) 덧칠하기.py
@@ -1,13 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/161989
 from collections import deque
-def solution(n, m, section):
-    answer = 0
-    section = deque(section)
+def solution(n, m , section):
+    answer = 0				
+    section = deque(section)	
+  
     while section:
-        start = section.popleft()
-        while section:
-            if section[0] >= start + m:
-                break
+        start = section.popleft()	# 페인트칠 시작
+        while section and start + m > section[0]: 
             section.popleft()
         answer += 1
+
     return answer
