01bfac4456351eeeff9a4f5794480b7ea0f18760
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-08-27 09:50:19 +0900

    Add files via upload

--- practice/할인 행사.py
+++ practice/할인 행사.py
@@ -1,35 +1,15 @@
 from collections import Counter
 def solution(want, number, discount):
     answer = 0
-    dic = {}
-    for i in range(len(want)):
-        dic[want[i]] = number[i]
-        
-    for i in range(len(discount)-9):
-        if dic == Counter(discount[i:i+10]): 
-            answer += 1
-   
-
-    return answer
-
-
-#---------------------------------
-# 직접 해결한 풀이
-# from collections import Counter
-# def solution(want, number, discount):
-#     answer = 0
     
-#     dic = {}
-#     for i,j in zip(want, number):
-#         dic[i] = j
-        
-#     for i in range(len(discount)):
-#         tmp = Counter(discount[i:i+10])
-#         count = 0
-#         for k,v in dic.items():
-#             if dic[k] <= tmp[k]: 
-#                 count += 1
-#         if count == len(want):
-#             answer += 1
- 
-#     return answer
+    check = {}
+    
+    for k,v in zip(want, number):
+        check[k] = v
+    
+    for i in range(len(discount)-9):
+        temp = Counter(discount[i:i+10])
+        if temp == check:
+            answer += 1
+    
+    return answer
