10318ee8473908667d50d1fa0185abd2ec0b1369
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-08-31 13:51:13 +0900

    Update 게임 맵 최단거리.py

--- 깊이·너비 우선 탐색(DFS·BFS)/게임 맵 최단거리.py
+++ 깊이·너비 우선 탐색(DFS·BFS)/게임 맵 최단거리.py
@@ -53,3 +53,35 @@ def solution(maps):
 #         return -1
 #     else: # 적 진영에 도착했을 경우 지나온 칸 출력
 #         return answer
+
+
+
+# 8/31
+# from collections import deque
+# def solution(maps):
+    
+#     dx = [0,0,-1,1]
+#     dy = [-1,1,0,0]
+    
+#     q = deque()
+#     q.append((0,0))
+    
+#     n, m = len(maps), len(maps[-1])
+    
+#     while q:
+#         x,y = q.popleft()
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if nx < 0 or nx >= n or ny < 0 or ny >= m:
+#                 continue
+#             if maps[nx][ny] == 1:
+#                 maps[nx][ny] = maps[x][y] + 1
+#                 q.append((nx,ny))
+    
+#     if maps[n-1][m-1] == 1:
+#         return -1
+#     else:
+#         return maps[n-1][m-1]
+    
+#     return answer
